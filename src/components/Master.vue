1c5551244b1fdb51552fa97eaf556c3a39b0b6ab
JosueMartinez <jmartinez@cdevteam.com>
2020-04-08 21:36:08 -0400

    including assets

---
+++ src/components/Master.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>this is a test</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'master'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
