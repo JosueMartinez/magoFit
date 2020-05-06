ef4d26d96497c195cfd1871fd587d5a9c7dc935e
JosueMartinez <jmartinez@cdevteam.com>
2020-05-06 12:37:20 -0400

    programs header

--- src/components/Program.vue
+++ src/components/Program.vue
@@ -1,12 +1,28 @@
 <template>
     <div>
-        <h1>this is Program</h1>
+         <SectionHeader :text="headerText" :color="headerColor" :image="headerImage" />
     </div>
 </template>
 
 <script>
+
+import SectionHeader from './Utils/TopSectionHeader'
+
 export default {
     name: 'Program',
+    components: {
+        SectionHeader
+    },
+    data(){
+        let isBaseball = this.$store.state.baseball; 
+        
+        return {
+            baseballData: isBaseball,
+            headerImage: isBaseball ? 'bb-programs-top.jpg' : 'fitness-programs-top.jpg',
+            headerText: "Programs",
+            headerColor: isBaseball ? 'black' : 'white'
+        }
+    }
 }
 </script>
 
